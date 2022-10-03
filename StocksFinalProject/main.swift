de557fcd061f93db5a162aca7ae2367c97a65189
Stainley Lebron <saiyamans@gmail.com>
2022-10-03 12:48:12 -0400

    Swift project

--- StocksFinalProject/main.swift
+++ StocksFinalProject/main.swift
@@ -7,5 +7,41 @@
 
 import Foundation
 
-print("Hello, World!")
 
+fileprivate class Main {
+    // Task 1
+    // Populate ten instances of StockHolding. Display all stocks sorted by company name in alphabetical order.
+    func printTenStockHolding() {
+        let stockService = StockServices()
+        
+        for stock in stockService.sortedByCompanyName() {
+            print(stock.printInformation(stockHolding: stock))
+        }
+    }
+
+    //print(printTenStockHolding())
+    // Task 2
+    func printTenAndForeignStock() {
+        let stockService = StockServices()
+        
+        for stock in stockService.sortedByCompanyNameReverse() {
+            print(stock.printInformation(stockHolding: stock))
+        }
+    }
+
+    // Task 3 Execute system with Menu
+    func execute() {
+        let runApp = MenuStockHolding()
+        runApp.displayMenu()
+    }
+}
+
+
+// TASK 1
+//Main().printTenStockHolding()
+
+// TASK 2
+//Main().printTenAndForeignStock()
+
+// TASK 3
+Main().execute()
